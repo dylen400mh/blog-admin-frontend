5dea6ebe64ad671290b6b0ade7de68cc9c36a634
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-07 15:18:35 -0400

    Remove isTokenExpired from imports

--- blog-admin-frontend/src/App.tsx
+++ blog-admin-frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import LoginForm from "./components/LoginForm";
 import PostList from "./components/PostList";
-import { isTokenExpired } from "./util/isTokenExpired";
 import { useAuth } from "./contexts/AuthContext";
 import Header from "./components/Header";
 
